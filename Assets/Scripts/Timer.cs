07652d87f8c95991054aef6a100f1187b101dc34
William Fermino <william.fermino@nxmultiservicos.com.br>
2020-01-06 23:03:32 -0300

    supermario merge

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text text;
+    long timer;
+    float duration = 1f;
+    float time = 0f;
+
+    void Awake()
+    {
+        timer = (1000 * 60) * 30;
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= duration)
+        {
+            time = 0f;
+            timer -= 1000;
+            text.text = "00:" + ((timer / 1000) / 60).ToString("00") + ":" + ((timer / 1000) % 60).ToString("00");
+        }
+    }
+}
